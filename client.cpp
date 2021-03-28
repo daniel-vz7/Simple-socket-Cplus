9b035f44867f19ca106eb36c317e677f9d53bf94
Danielvvz <danieltux@outlook.com>
2021-03-27 21:07:10 -0600

    Connect sockets
    - Connect server and client

--- client.cpp
+++ client.cpp
@@ -1,3 +1,4 @@
+#include <vector>
 #include <iostream>
 #include <string>
 #include <stdio.h>
@@ -14,10 +15,11 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <fstream>
+#include <sstream>
+
 using namespace std;
 
-//Client side
-int main(int argc, char *argv[]) {
+void printMenu(void) {
   // Display menu
   cout << "Ingresa el dato que deseas obtener" << endl;
   cout << "1. Nombre de pelicula" << endl;
@@ -28,6 +30,35 @@ int main(int argc, char *argv[]) {
   cout << "6. Rotten Tomatoes" << endl;
   cout << "7. Ganancia global" << endl;
   cout << "8. Año" << endl;
+}
+
+//Client side
+int main(int argc, char *argv[]) {
+  //we need 2 things: ip address and port number, in that order
+  if (argc != 3) {
+    cerr << "Usage: ip_address port" << endl;
+    exit(0);
+  } //grab the IP address and port number
+  char *serverIp = argv[1];
+  int port = atoi(argv[2]);
+  char message[1500];
+
+  //setup a socket and connection tools
+  struct hostent *host = gethostbyname(serverIp);
+  sockaddr_in sendSockAddr;
+  bzero((char *)&sendSockAddr, sizeof(sendSockAddr));
+  sendSockAddr.sin_family = AF_INET;
+  sendSockAddr.sin_addr.s_addr = inet_addr(inet_ntoa(*(struct in_addr *)*host->h_addr_list));
+  sendSockAddr.sin_port = htons(port);
+  int clientSd = socket(AF_INET, SOCK_STREAM, 0);
+  //try to connect...
+  int status = connect(clientSd, (sockaddr *)&sendSockAddr, sizeof(sendSockAddr));
+  if (status < 0) {
+    cout << "Error connecting to socket!" << endl;
+    exit(1);
+  }
+
+  printMenu();
   // Enter option
   cout << "Ingresa el dato a obtener" << endl << ">";
   string option;
@@ -36,6 +67,31 @@ int main(int argc, char *argv[]) {
   cout << "Ingresa el nombre de la pelicula" << endl << ">";
   string filmName;
   getline(cin, filmName);
+
+  // Prepare consult
+  string instruction = option + "," + filmName;
+  strcpy(message, instruction.c_str());
+  send(clientSd, (char *)&message, strlen(message), 0);
+  memset(&message, 0, sizeof(message)); //clear the buffer
+
+  // Receive response from server
+  recv(clientSd, (char *)&message, sizeof(message), 0);
+  vector <string> tokens; 
+  stringstream check1(message); 
+  string intermediate; 
+  // Tokenizing w.r.t. space ',' 
+  while (getline(check1, intermediate, ',')) tokens.push_back(intermediate);
+
+  stringstream geek(tokens[0]);
+  int operationCode = 0;
+  geek >> operationCode;
+  string response;
+
+  // Display response
+  if (operationCode == 1) cout << "La respuesta es: " << tokens[1] << endl;
+  else cout << "No se encontro la pelicula" << endl;
+  // Close connection
+  close(clientSd);
 
   return 0;
 }
--- server.cpp
+++ server.cpp
@@ -21,8 +21,14 @@ using namespace std;
 
 // Server side
 int main(int argc, char *argv[]) {
+  // For the server, we only need to specify a port number
+  if (argc != 2) {
+    cerr << "Usage: port" << endl;
+    exit(0);
+  }
+
   // Load file
-  string data[78][8];
+  string moviesData[78][8];
   int flag = 0;
   ifstream input("movies.csv");
   for (string line; getline(input, line);) {
@@ -35,11 +41,78 @@ int main(int argc, char *argv[]) {
     } 
     // Iterating over row fields
     for (int i = 0; i < tokens.size(); i++) {
-		  cout << "i " << i  << " -> " << tokens[i] << '\n'; 
-      data[flag][i] = tokens[i];
+      moviesData[flag][i] = tokens[i];
     }
     flag++;
   }
+
+  // Grab the port number
+  int port = atoi(argv[1]);
+  // Buffer to send and receive messages with
+  char message[1500];
+
+  // Setup a socket and connection tools
+  sockaddr_in servAddr;
+  bzero((char *)&servAddr, sizeof(servAddr));
+  servAddr.sin_family = AF_INET;
+  servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servAddr.sin_port = htons(port);
+
+  // Open stream
+  int serverSd = socket(AF_INET, SOCK_STREAM, 0);
+  if (serverSd < 0) {
+    cerr << "Error establishing the server socket" << endl;
+    exit(0);
+  }
+  //bind the socket to its local address
+  int bindStatus = bind(serverSd, (struct sockaddr *)&servAddr, sizeof(servAddr));
+  if (bindStatus < 0) {
+    cerr << "Error binding socket to local address" << endl;
+    exit(0);
+  }
+
+  // Listen to requests
+  listen(serverSd, 5);
+  // New address to connect with the client
+  sockaddr_in newSockAddr;
+  socklen_t newSockAddrSize = sizeof(newSockAddr);
+  int newSd = accept(serverSd, (sockaddr *)&newSockAddr, &newSockAddrSize);
+  if (newSd < 0) {
+    cerr << "Error accepting request from client!" << endl;
+    exit(0);
+  }
+
+  // Receive instructions from client
+  memset(&message, 0, sizeof(message)); 
+  recv(newSd, (char *)&message, sizeof(message), 0);
+  vector <string> tokens; 
+  stringstream check1(message); 
+  string intermediate; 
+  // Tokenizing w.r.t. space ',' 
+  while (getline(check1, intermediate, ',')) tokens.push_back(intermediate);
+
+  stringstream geek(tokens[0]);
+  int option = 0;
+  int operationCode = 0;
+  geek >> option;
+  string response;
+
+  // Search film
+  for (int i = 1; i < 78; i++) {
+    if (tokens[1] == moviesData[i][0]) {
+      response = moviesData[i][option - 1];
+      operationCode = 1;
+      break;
+    }
+  }
+  
+  // Send response to client
+  response = to_string(operationCode) + "," + response;
+  memset(&message, 0, sizeof(message)); //clear the buffer
+  strcpy(message, response.c_str());
+  send(newSd, (char *)&message, strlen(message), 0);
+  close(newSd);
+  close(serverSd);
 
   return 0;
 }
